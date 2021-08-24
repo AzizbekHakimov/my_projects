17dac32b308d729b3f1f9559aff1b33b76fe87bf
AzizbekHakimov <78505265+AzizbekHakimov@users.noreply.github.com>
2021-08-24 23:05:50 +0500

    Add files via upload

---
+++ Assignment2.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[3]:
+
+
+print("Which vegetables do you want to buy? ")
+vegetable = input()
+print("Which fruits do you want to buy? ")
+fruits = input()
+print("Which drinks do you want to buy? ")
+drinks = input()
+print("What other things do you want to buy? ")
+other = input()
+
+print(f"You bought {vegetable}, {fruits}, {drinks} and {other}.\nThank you for shopping! ")
+
+
+# In[ ]:
+
+
+
+
